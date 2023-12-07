8e233d46e758116fe4930e491d27ed31bfa43321
Anupam Kumar <kyteinsky@gmail.com>
2023-12-07 18:31:04 +0530

    update screenshots and dynamic import fix
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { handleNotification } from './notification.js'
 
 import { subscribe } from '@nextcloud/event-bus'
+import { linkTo } from '@nextcloud/router'
+import { getRequestToken } from '@nextcloud/auth'
 
-(function() {
+__webpack_nonce__ = btoa(getRequestToken()) // eslint-disable-line
+__webpack_public_path__ = linkTo('stt_helper', 'js/') // eslint-disable-line
+
+;(function() {
 	subscribe('notifications:action:execute', handleNotification)
 })()
--- src/resultPage.js
+++ src/resultPage.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
+
+import { linkTo } from '@nextcloud/router'
+import { getRequestToken } from '@nextcloud/auth'
+
 import ResultPage from './views/ResultPage.vue'
+
+__webpack_nonce__ = btoa(getRequestToken()) // eslint-disable-line
+__webpack_public_path__ = linkTo('stt_helper', 'js/') // eslint-disable-line
+
 Vue.mixin({ methods: { t, n } })
 
 const View = Vue.extend(ResultPage)
